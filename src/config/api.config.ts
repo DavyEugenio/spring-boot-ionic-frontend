137ac8a45d71a64719bdb7444345d88ca3a3e6ab
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-03-17 14:14:48 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,1 +1,4 @@
-export const API_CONFIG = { baseUrl: "http://localhost:8070" }
+export const API_CONFIG = {
+    baseUrl: "http://localhost:8070",
+    bucketBaseUrl: "https://curso-spring-ionic-deno.s3-sa-east-1.amazonaws.com"
+}
