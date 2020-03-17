cdb0d49c24a44b5a6c1560c4397b9508551e34ed
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-03-17 20:15:25 -0300

    Mostrando dados e imagem do cliente para pagina de profile

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:8070",
-    bucketBaseUrl: "https://curso-spring-ionic-deno.s3-sa-east-1.amazonaws.com"
+    bucketBaseUrl: "https://curso-spring-ionic-deno.s3.sa-east-1.amazonaws.com"
 }
