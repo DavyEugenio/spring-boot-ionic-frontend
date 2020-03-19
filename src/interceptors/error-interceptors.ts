da6b4b26158e985050f57b89748b929debf88c9d
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-03-19 01:14:42 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptors.ts
+++ src/interceptors/error-interceptors.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
 import { AlertController, Alert } from 'ionic-angular';
+import { FieldMessage } from "../models/fieldmessage";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -28,6 +29,9 @@ export class ErrorInterceptor implements HttpInterceptor {
                         break;
                     case 403:
                         this.handle403();
+                        break;
+                    case 422:
+                        this.handle422(errorObj);
                         break;
                     default:
                         this.handleDefaultError(errorObj);
@@ -56,6 +60,21 @@ export class ErrorInterceptor implements HttpInterceptor {
         this.storage.setLocalUser(null);
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create(
+            {
+                title: 'Erro 422: Validação',
+                message: this.listErrors(errorObj.errors),
+                enableBackdropDismiss: false,
+                buttons:
+                    [
+                        { text: 'Ok' }
+                    ]
+            }
+        );
+        alert.present();
+    }
+
     handleDefaultError(errorObj) {
         let alert = this.alertCtrl.create(
             {
@@ -69,6 +88,14 @@ export class ErrorInterceptor implements HttpInterceptor {
             }
         );
         alert.present();
+    }
+
+    private listErrors(messages: FieldMessage[]) : string{
+        let s: string = '';
+        for (var i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + '</strong>: ' + messages[i].message + '</p>';
+        }
+        return s;
     }
 }
 
