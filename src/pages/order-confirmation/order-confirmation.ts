47557e9ee958d32904fd3d2e2fea73242153ea30
Davy Eugenio N Oliveira <davy.eugenio@hotmail.com>
2020-03-21 04:33:10 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service.dto';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
     this.pedido = this.navParams.get('pedido');
   }
 
@@ -34,9 +36,9 @@ export class OrderConfirmationPage {
         this.cliente = response as ClienteDTO;
         this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
       },
-      error => {
-        this.navCtrl.setRoot('HomePage');
-      }
+        error => {
+          this.navCtrl.setRoot('HomePage');
+        }
       );
   }
 
@@ -45,7 +47,25 @@ export class OrderConfirmationPage {
     return list[position];
   }
 
-  total(){
+  total() {
     return this.cartService.total();
+  }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        console.log(response.headers.get('location'));
+        this.cartService.createOrClearCart();
+      },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        }
+      );
   }
 }
--- src/services/domain/categoria.service.ts
+++ src/services/domain/categoria.service.ts
@@ -6,6 +6,7 @@ import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class CategoriaService {
+    
     constructor(public http: HttpClient){
     }
 
